268613ba2758bd32954f82ec3eab5b6eea404703
Prabhanjan Kumar Mahapatra <mahapatra.prabhanjan@hotmail.com>
2018-11-25 07:59:49 +0530

    Project setup 2nd commit

---
+++ graph_console/graph_v1/graph_v1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace graph_v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+        }
+    }
+}
