ed4d2a068c395b2852ced0c19d010c19c40f472b
Mahapatra, Prabhanjan <prabhanjan.mahapatra@fnf.com>
2018-11-26 01:37:13 +0000

    Added MsalAuthentication Provider & Authentication Handler

---
+++ graph_console/graph_v1/graph_v1/Helpers/AuthenticationHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace graph_v1.Helpers
+{
+    public class AuthenticationHandler : DelegatingHandler
+    {
+        private readonly IAuthenticationProvider _authenticationProvider;
+
+        protected AuthenticationHandler(IAuthenticationProvider authenticationProvider, HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+            _authenticationProvider = authenticationProvider;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            await _authenticationProvider.AuthenticateRequestAsync(request);
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ graph_console/graph_v1/graph_v1/Helpers/MsalAuthenticationProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace graph_v1.Helpers
+{
+    public class MsalAuthenticationProvider : IAuthenticationProvider
+    {
+        private ConfidentialClientApplication _clientApplication;
+        private string[] _scopes;
+
+        public MsalAuthenticationProvider(ConfidentialClientApplication clientApplication, string[] scopes)
+        {
+            _clientApplication = clientApplication;
+            _scopes = scopes;
+        }
+
+        public async Task AuthenticateRequestAsync(HttpRequestMessage request)
+        {
+            var token = await GetTokenAsync();
+            request.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
+        }
+
+        public async Task<string> GetTokenAsync()
+        {
+            AuthenticationResult authResult = await _clientApplication.AcquireTokenForClientAsync(_scopes);
+            return authResult.AccessToken;
+        }
+    }
+}
