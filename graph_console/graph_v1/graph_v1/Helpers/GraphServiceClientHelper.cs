3a5f63c01cd3011d6a69816685f9c69dddee4e67
Mahapatra, Prabhanjan <prabhanjan.mahapatra@fnf.com>
2018-11-26 01:56:07 +0000

    Configuration update

---
+++ graph_console/graph_v1/graph_v1/Helpers/GraphServiceClientHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace graph_v1.Helpers
+{
+    public class GraphServiceClientHelper
+    {
+        public static IAuthenticationProvider CreateAuthorizationProvider(IConfigurationRoot config)
+        {
+            var clientId = config["ClientId"];
+            var clientSecret = config["ClientSecret"];
+            var redirectUri = config["RedirectUri"];
+            var authorityUrl = config["AuthorityUrl"];
+
+            var scopes = new List<string>();
+            scopes.Add("user.read");
+            scopes.Add("calendars.read");
+            scopes.Add("calendars.readwrite");
+            scopes.Add("tasks.read");
+            scopes.Add("tasks.readwrite");
+        }
+    }
+}
--- graph_console/graph_v1/graph_v1/Program.cs
+++ graph_console/graph_v1/graph_v1/Program.cs
@@ -1,19 +1,26 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
 using System;
-using System.IO;
+using FileSystemDirectory = System.IO.Directory;
+using static System.Console;
+using System.Net.Http;
 
 namespace graph_v1
 {
     class Program
     {
+        private static GraphServiceClient _graphServiceClient;
+        private static HttpClient _httpClient;
+
         static void Main(string[] args)
         {
             var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
+                .SetBasePath(FileSystemDirectory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
             IConfigurationRoot configuration = builder.Build();
-
+            if (configuration == null)
+                WriteLine("Missing or invalid appsettings.json.");
         }
     }
 }
